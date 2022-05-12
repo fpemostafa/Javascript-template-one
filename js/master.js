5a40a465582b0b0386b2c0de0c690236b17bef2a
Mostafa Mahmoud <eng.mostafa.820@gmail.com>
2022-05-12 03:18:29 -0700

    adding Skill Section design and adding percentage feature to the Skill progress bar with some awesome animation

--- js/master.js
+++ js/master.js
@@ -54,18 +54,17 @@ colorLi.forEach((li) => {
 
 //-----------------------------------------------------------------------------------
 
-
 // Random Background Option
 //.
 //.
 //.
-// randomBoolean to control randomize Images Function 
+// randomBoolean to control randomize Images Function
 let randomBackground = true;
 
-// Variable to control the Interval 
+// Variable to control the Interval
 let theBackgroundInterval;
 
-// cheek if the is random-backgrounds value stored in local Storage 
+// cheek if the is random-backgrounds value stored in local Storage
 let storedRandomBackground = localStorage.getItem("random-backgrounds");
 // console.log(storedRandomBackground);
 
@@ -73,13 +72,15 @@ if (storedRandomBackground !== null) {
     if (storedRandomBackground === "true") {
         randomBackground = true;
         randomizeImgs();
-        document.querySelector(".background-toggle span.yes").classList.add("active");
+        document
+            .querySelector(".background-toggle span.yes")
+            .classList.add("active");
     } else {
         randomBackground = false;
-        document.querySelector(".background-toggle span.no").classList.add("active");
-
+        document
+            .querySelector(".background-toggle span.no")
+            .classList.add("active");
     }
-
 }
 
 // randomBackgroundSpan var will give us a list of spans located in background-toggle class
@@ -95,7 +96,6 @@ randomBackgroundSpan.forEach((span) => {
         // remove active class from all elements
         e.target.parentElement.querySelectorAll(".active").forEach((element) => {
             element.classList.remove("active");
-
         });
         // add active Class to current Element
         e.target.classList.add("active");
@@ -113,11 +113,6 @@ randomBackgroundSpan.forEach((span) => {
     });
 });
 
-
-
-
-
-
 // function to randomize imgs
 function randomizeImgs() {
     // Landing Page Selection
@@ -133,6 +128,56 @@ function randomizeImgs() {
 
             // Change background img url
             LandingPage.style.backgroundImage = `url(../imgs/${imgsArray[randomNumber]})`;
-        }, 1 * 1000);
+        }, 10 * 1000);
     }
 }
+
+// Select Skill Section
+let ourSkill = document.querySelector(".skills");
+
+window.onscroll = function() {
+    //* Skill Offset Top => The total Height above our Parent Element
+    let skillOffsetTop = ourSkill.offsetTop;
+    // console.log(`Skill Offset Top => ${skillOffsetTop}`);
+
+    //* Skill Outer Height => the total amount of height that Element Take * check MDN website for more information
+    let skillOuterHeight = ourSkill.offsetHeight;
+    // console.log(`Skill Outer Height => ${skillOuterHeight}`);
+
+    //* Window Height
+    let windowHeight = this.innerHeight;
+    // console.log(`Window Height => ${windowHeight}`);
+
+    //* Window  Scroll Top => the total amount of height that we already scrolled and goes out of the page view
+    let windowScrolled = this.scrollY;
+    console.log(`Window  Scroll Top => ${windowScrolled}`);
+
+    //* Learning Function by Osama Elzero
+    // if (windowScrolled > (skillOffsetTop + skillOuterHeight - windowHeight)) {
+    //     let allSkills = document.querySelectorAll(".skill-box .skill-progress span");
+
+    //     allSkills.forEach(span => {
+    //         span.style.width = span.dataset.progress;
+    //         span.firstElementChild.textContent = span.dataset.progress;
+    //         console.log(span.firstElementChild);
+
+    //     })
+
+    // }
+    //* My Own Function
+    let allSkillPercentage = document.querySelectorAll(".skill-percentage");
+    allSkillPercentage.forEach((element) => {
+        elePercentage = element.parentElement.dataset.progress;
+        if (windowScrolled > skillOffsetTop + skillOuterHeight - windowHeight) {
+            element.parentElement.style.width = elePercentage;
+            element.textContent = elePercentage;
+            if (elePercentage <= "2%") {
+                element.style.fontSize = "12px";
+            }
+        } else {
+            element.parentElement.style.width = 0;
+            element.textContent = "";
+        }
+        //   console.log(elePercentage);
+    });
+};
